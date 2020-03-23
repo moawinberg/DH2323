3f931104af6f2b214dd61b94504f5e1ec9f1eeac
Moa <moa.winberg.1@gmail.com>
2020-03-23 11:00:00 +0100

    add files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DH2323
+//
+//  Created by moa on 2020-03-23.
+//  Copyright © 2020 moa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
